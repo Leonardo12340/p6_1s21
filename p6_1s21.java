4309cd3690565545402a35c26eca0a9fbc87a18b
Leonardo12340 <144502307+Leonardo12340@users.noreply.github.com>
2023-09-28 21:52:28 -0600

    Add files via upload

---
+++ p6_1s21.java
@@ -0,0 +1,11 @@
+class p6_1s21{
+	public static void main(String[] args) {
+		String nombre= "leonardo";
+		int edad= 19;
+		System.out.println("el valor para edad es" + nombre);
+		System.out.println("la edad es" + edad);
+		System.out.println("puede proseguir programando");
+	}
+}
+		
+	
